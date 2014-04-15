1ce5333f39a50da4d8a94255e2b5f2f28440742c
Ruari <ruari@charliefortune.com>
2014-04-15 14:20:38 +0100

    created a homeview class and moved functionality across. Added a banner template

---
+++ js/HomeView.js
@@ -0,0 +1,26 @@
+var HomeView = function(store) {
+ 
+	 this.initialise = function(store){
+	 	this.el = $('<div/>');
+	 	this.el.on('keyup','.search-key',this.findByName);
+	 }
+
+	 this.findByName = function() {
+    	var self = this;
+    	store.findByName($('.search-key').val(), function(players) {
+    	    $('.player-list').html(HomeView.playerLiTpl(players));
+    	});
+    }
+
+	 this.render = function() {
+    	this.el.html(HomeView.bannerTpl() + HomeView.homeTpl());
+    	return this;
+    }
+
+ this.initialise();
+ 
+}
+
+HomeView.bannerTpl = Handlebars.compile($("#header-banner-tpl").html());
+HomeView.homeTpl = Handlebars.compile($("#home-tpl").html());
+HomeView.playerLiTpl = Handlebars.compile($("#player-li-tpl").html());
