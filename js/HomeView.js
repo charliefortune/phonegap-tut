5de53725c281a84f897ed35fe4c4d172d7cafafb
Charlie Fortune <ruari@charliefortune.com>
2014-04-28 20:33:59 +0100

    added an alert for debugging

--- js/HomeView.js
+++ js/HomeView.js
@@ -12,6 +12,7 @@ var HomeView = function(store) {
 
     this.findContact = function(event) {
 	event.preventDefault();
+	var self = this;
 	//console.log('findContact');
 	if (!navigator.contacts) {
 	    this.showAlert("Contacts API not supported", "Error");
@@ -25,6 +26,7 @@ var HomeView = function(store) {
 	navigator.contacts.find(fields, 
 	    function onSuccess(contacts) {
                 var msg = 'Found ' + contacts.length + ' contacts.';
+		app.showAlert(msg, "Notice");
 		if(contacts.length > 0){
 		    console.log(msg);
                     $(".contact-list").html(HomeView.contactLiTpl(contacts));
